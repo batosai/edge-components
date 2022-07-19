24c0682569d0bd1703dc833ee074f506757028de
jeremy <jeremy@chaufourier.fr>
2022-07-19 23:07:21 +0200

    Init fractal project

---
+++ docs/01-index.md
@@ -0,0 +1,21 @@
+---
+title: edge daisyUI
+---
+
+The most popular, free and open-source Tailwind CSS component library in edge template engine.
+
+- [ ] Alert
+- [ ] Avatar
+- [ ] Badge
+- [ ] Button(WIP)
+- [ ] Card
+- [ ] Carousel
+- [ ] Drawer
+- [ ] Dropdown(WIP)
+- [ ] Modal
+- [ ] Pagination
+- [ ] Progress
+- [ ] Tab
+- [ ] Table
+- [ ] Toast
+- [ ] Tooltip(WIP)
---
+++ fractal.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+const mandelbrot = require('@frctl/mandelbrot');
+
+// create a new instance with custom config options
+const myCustomisedTheme = mandelbrot({
+  skin: 'white',
+  // any other theme configuration values here
+});
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'Daisyui');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'templates'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+// tell Fractal to use the configured theme by default
+fractal.web.theme(myCustomisedTheme);
+
+fractal.components.set('default.status', 'wip');
+fractal.docs.set('default.status', 'draft');
+
+fractal.components.set('ext', '.edge'); // look for files with a .nunj file extension
+fractal.components.set('edge.components.path', path.join(__dirname, 'views'));
+fractal.components.engine('@jrmc/fractal-edge-adapter'); // use the configured Nunjucks instance for components
