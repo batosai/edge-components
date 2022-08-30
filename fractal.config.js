94448436a55798e724a416a1dae855ec2f7e16ae
jeremy <jeremy@chaufourier.fr>
2022-08-30 23:48:21 +0200

    Refacto

--- fractal.config.js
+++ fractal.config.js
@@ -74,8 +74,8 @@ fractal.components.set('edge.helpers', {
       return [baseClassString, klassString].join(' ').trim()
     },
     getTagName: (props, defaultTagName='div') => props.has('as') ? props.get('as') : defaultTagName,
-    getName: props => props.has('name') ? props.get('name') : '',
-    getId: props => props.has('name') ? props.get('name') : '',
+    getName: (props, context={name: false}) => props.has('name') || context.name ? props.get('name') || context.name : '',
+    getId: (props, context={id: false}) => props.has('id') || context.id || props.has('name') ? props.get('id') || context.id || props.get('name') : '',
     getRequired: (props, context={required: false}) => props.has('required') || context.required ? props.get('required') || context.required : false,
     getLabel: props => {
       const name = props.has('name') ? props.get('name') : ''
