3c283e367864aee0fdc1dbda4544e52a48d719e3
jeremy <jeremy@chaufourier.fr>
2022-09-15 23:30:13 +0200

    Mouve fractal app and prefix components

--- fractal.config.js
+++ fractal.config.js
@@ -36,22 +36,22 @@ const fractal = module.exports = require('@frctl/fractal').create();
  * Give your project a title.
  */
 fractal.set('project.title', 'edge-components');
-fractal.web.set('builder.dest', __dirname + '/build');
+fractal.web.set('builder.dest', __dirname + '/fractal/build');
 
 /*
  * Tell Fractal where to look for components.
  */
-fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set('path', path.join(__dirname, 'fractal/components'));
 
 /*
  * Tell Fractal where to look for documentation pages.
  */
-fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set('path', path.join(__dirname, 'fractal/docs'));
 
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', path.join(__dirname, 'public'));
+fractal.web.set('static.path', path.join(__dirname, 'fractal/public'));
 
 // tell Fractal to use the configured theme by default
 fractal.web.theme(myCustomisedTheme);
@@ -60,6 +60,7 @@ fractal.components.set('default.status', 'wip');
 fractal.docs.set('default.status', 'draft');
 
 fractal.components.set('ext', '.edge'); // look for files with a .nunj file extension
+fractal.components.set('edge.components.prefix', 'jrmc');
 fractal.components.set('edge.components.path', path.join(__dirname, 'views'));
 
 const capitalize = text => (text[0].toUpperCase() + text.substring(1))
