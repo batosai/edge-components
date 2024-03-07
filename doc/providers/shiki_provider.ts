b53ccb8a5b55bb7edbe0f762236685520c266b6d
jeremy <jeremy@chaufourier.fr>
2024-03-08 00:43:58 +0100

    Init new doc

---
+++ doc/providers/shiki_provider.ts
@@ -0,0 +1,32 @@
+import type { ApplicationService } from '@adonisjs/core/types'
+
+export default class ShikiProvider {
+  constructor(protected app: ApplicationService) {}
+
+  /**
+   * Register bindings to the container
+   */
+  register() {}
+
+  /**
+   * The container bindings have booted
+   */
+  async boot() {
+    await import('./Shiki/index.js')
+  }
+
+  /**
+   * The application has been booted
+   */
+  async start() {}
+
+  /**
+   * The process has been started
+   */
+  async ready() {}
+
+  /**
+   * Preparing to shutdown the app
+   */
+  async shutdown() {}
+}
---
+++ doc/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+import edgeComponent from '../build/tailwind.config'
+
+export default {
+  content: [
+    './resources/views/**/*.edge',
+    ...edgeComponent.content
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    ...edgeComponent.plugins,
+  ],
+}
+
