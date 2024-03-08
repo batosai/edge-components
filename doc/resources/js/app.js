5f573aa5087729838be4c03ed742de413078565b
jeremy <jeremy@chaufourier.fr>
2024-03-09 00:40:17 +0100

    WIP prepare template

--- doc/resources/js/app.js
+++ doc/resources/js/app.js
@@ -1,1 +1,19 @@
 import '../css/app.css'
+import 'unpoly/unpoly.css'
+
+import 'unpoly'
+import Alpine from 'alpinejs'
+
+
+// Unpoly
+up.link.config.followSelectors.push('a[href]')
+up.form.config.submitSelectors.push(['form'])
+up.feedback.config.currentClasses.push(['active'])
+up.feedback.config.navSelectors.push(['nav'])
+
+up.layer.config.drawer.size = 'large'
+up.layer.config.drawer.position = 'right'
+
+
+// Alpine
+Alpine.start()
--- doc/start/view.ts
+++ doc/start/view.ts
@@ -1,14 +1,10 @@
 
 import edge from 'edge.js'
 import { edgeIconify, addCollection } from 'edge-iconify'
-import { icons as akarIcons } from '@iconify-json/akar-icons'
-import { icons as faIcons } from '@iconify-json/fa-solid'
 import { icons as tablerIcons } from '@iconify-json/tabler'
 // import { migrate } from 'edge.js/plugins/migrate'
 import jrmc from '../../build/index.js'
 
-addCollection(akarIcons)
-addCollection(faIcons)
 addCollection(tablerIcons)
 
 edge.use(jrmc)
