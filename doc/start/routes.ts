7cd4d24524a5a7d28b6068791b98b5d0ccce257e
jeremy <jeremy@chaufourier.fr>
2024-03-09 18:30:03 +0100

    WIP

--- doc/start/routes.ts
+++ doc/start/routes.ts
@@ -9,4 +9,9 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+router.on('/changelog').render('pages/changelog').as('changelog')
+router.on('/button').render('pages/button').as('button')
+router.on('/drawer').render('pages/drawer').as('drawer')
+router.on('/dropdown').render('pages/dropdown').as('dropdown')
+router.on('/modal').render('pages/modal').as('modal')
