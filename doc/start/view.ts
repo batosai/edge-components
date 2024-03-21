ee7d11383e3705be0d87d2d18d9407385654a11b
jeremy <jeremy@chaufourier.fr>
2024-03-22 00:34:12 +0100

    Add cookie button

--- doc/start/view.ts
+++ doc/start/view.ts
@@ -2,9 +2,9 @@
 import edge from 'edge.js'
 import { edgeIconify, addCollection } from 'edge-iconify'
 import { icons as tablerIcons } from '@iconify-json/tabler'
-// import { edgePluginComponent } from '../../build/index.js'
+import { icons as fluentIcons } from '@iconify-json/fluent'
 
 addCollection(tablerIcons)
+addCollection(fluentIcons)
 
-// edge.use(edgePluginComponent)
 edge.use(edgeIconify)
